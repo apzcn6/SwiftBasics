e16fc993398607cd8745476b02028e926616d3fd
Andrew Ziber <apzcn6@mail.missouri.edu>
2019-09-06 10:31:08 -0500

    Finished bool part

--- SwiftBasics/main.swift
+++ SwiftBasics/main.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-print("Hello, World!")
-
 let sample1: UInt8 = 0x3A
 
 var sample2:UInt8 = 58
@@ -26,6 +24,8 @@ var distance: Double = (129.763001)
 
 var lost: Bool = (true)
 
+var expensive: Bool = (true)
+
 var choice: Int = (2)
 
 let integral: Character = "\u{222B}"
@@ -40,5 +40,28 @@ if sample1 == sample2{
     print("The samples are not equal")
 }
 
+if heartrate > 40 && heartrate <= 80{
+    print("Heart rate is normal")
+}
+else{
+    print("Heart rate is not normal")
+}
+
+if deposits >= 100000000{
+    print("You are exceedingly wealthy")
+}else{
+    print("Sorry you are so poor")
+}
+
+var force: Float = mass * accleration
+print("force = \(force)")
+
+print("\(distance) is the distance")
 
 
+if lost == true && expensive == true{
+    print("I am really sorry I will get the manager")
+}
+if lost == true && expensive == false{
+    print("Here is coupon for 10% off")
+}
