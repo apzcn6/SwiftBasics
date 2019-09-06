ac2e26bc353db76050af9ad1b39af69e92444c85
Andrew Ziber <apzcn6@mail.missouri.edu>
2019-09-06 10:11:56 -0500

    First commit

---
+++ SwiftBasics/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Andrew Ziber on 9/6/19.
+//  Copyright © 2019 Andrew Ziber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let sample1: UInt8 = 0x3A
+
+var sample2:UInt8 = 58
+
+var heartrate: Int = (85)
+
+var deposits: Double = (135002796)
+
+let accleration: Float = (9.800)
+
+var mass: Float = (14.6)
+
+var distance: Double = (129.763001)
+
+var lost: Bool = (true)
+
+var choice: Int = (2)
+
+let integral: Character = "\u{222B}"
+
+let greeting: String = "Hello"
+
+var name: String = "Karen"
+
+if sample1 == sample2{
+    print("The samples are equal")
+}else{
+    print("The samples are not equal")
+}
+
+
+
